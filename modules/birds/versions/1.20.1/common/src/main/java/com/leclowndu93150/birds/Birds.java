a5eb699ce88d30b3aaaf7e0f7ee583a6236f4c18
Leclowndu93150 <xdr.a123454321@gmail.com>
2026-04-17 09:02:21 +0200

    add pack.mcmeta bs

--- modules/birds/versions/1.20.1/common/src/main/java/com/leclowndu93150/birds/Birds.java
+++ modules/birds/versions/1.20.1/common/src/main/java/com/leclowndu93150/birds/Birds.java
@@ -6,11 +6,12 @@ import net.minecraft.world.item.Item;
 
 public class Birds {
     public static final String MOD_ID = "birds";
-
+    public static long sixseven2 = 67;
     public static EntityType<?> BIRD;
     public static Item BIRD_ITEM;
 
     public static ResourceLocation id(String path) {
+        sixseven2 += 1;
         return new ResourceLocation(MOD_ID, path);
     }
 }
